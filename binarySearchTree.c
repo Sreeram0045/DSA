7e7e8e456dbdb6da5f24146264ea1851f99a593f
Sreeram0045 <sreeramjmenon10@gmail.com>
2025-10-26 23:21:14 +0530

    feat: Implemented single child and no child deletion

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -10,8 +10,8 @@ struct node {
   NODE *right;
 };
 
-NODE *ROOT = NULL;
-bool isEmpty() { return ROOT == NULL; }
+bool isEmpty(NODE *ROOT) { return ROOT == NULL; }
+
 NODE *createNewNode(int num) {
   NODE *newNode = (NODE *)malloc(sizeof(NODE));
   if (newNode == NULL) {
@@ -22,35 +22,91 @@ NODE *createNewNode(int num) {
   newNode->num = num;
   return newNode;
 }
-NODE *recursiveSearchingForPosition(int num, NODE *pointer) {
-  if (pointer == NULL || pointer->num == num) {
+NODE *recursiveSearchingForPosition(int num, NODE **pointer) {
+  if (*pointer == NULL || (*pointer)->num == num) {
     return createNewNode(num);
-  } else if (num < pointer->num) {
-    pointer->left = recursiveSearchingForPosition(num, pointer->left);
-  } else if (num > pointer->num) {
-    pointer->right = recursiveSearchingForPosition(num, pointer->right);
+  } else if (num < (*pointer)->num) {
+    (*pointer)->left = recursiveSearchingForPosition(num, &(*pointer)->left);
+  } else if (num > (*pointer)->num) {
+    (*pointer)->right = recursiveSearchingForPosition(num, &(*pointer)->right);
   }
-  return pointer;
+  return *pointer;
 }
-void insertNewNode() {
+void insertNewNode(NODE **ROOT) {
   int num;
   printf("Enter the number: ");
   scanf("%d", &num);
-  ROOT = recursiveSearchingForPosition(num, ROOT);
+  *ROOT = recursiveSearchingForPosition(num, ROOT);
 }
-void printInorder(NODE *pointer) {
+NODE *deleteNode(int num, NODE **ROOT) {
+  if (num == (*ROOT)->num || ROOT == NULL) {
+    if ((*ROOT)->left == NULL && (*ROOT)->right == NULL) {
+      printf("Number deleted\n");
+      free(*ROOT);
+      return NULL;
+    } else {
+      if ((*ROOT)->left == NULL) {
+        (*ROOT)->num = ((*ROOT)->right)->num;
+        printf("Number deleted from right node\n");
+        free((*ROOT)->right);
+        (*ROOT)->right = NULL;
+      } else if ((*ROOT)->right == NULL) {
+        printf("Number deleted from left node\n");
+        (*ROOT)->num = ((*ROOT)->left)->num;
+        free((*ROOT)->left);
+        (*ROOT)->left = NULL;
+      }
+    }
+  } else if ((*ROOT)->num < num) {
+    (*ROOT)->right = deleteNode(num, &(*ROOT)->right);
+  } else {
+    (*ROOT)->left = deleteNode(num, &(*ROOT)->left);
+  }
+  return *ROOT;
+}
+void deleteNodeCaller(NODE **ROOT) {
+  int num;
+  printf("Enter the number for deletion: ");
+  scanf("%d", &num);
+  deleteNode(num, &*ROOT);
+}
+void inorderTraversal(NODE *pointer) {
   if (pointer != NULL) {
-    printInorder(pointer->left);
+    inorderTraversal(pointer->left);
     printf("%d ", pointer->num);
-    printInorder(pointer->right);
+    inorderTraversal(pointer->right);
+  }
+}
+void preorderTraversal(NODE *pointer) {
+  if (pointer != NULL) {
+    printf("%d ", pointer->num);
+    preorderTraversal(pointer->left);
+    preorderTraversal(pointer->right);
+  }
+}
+void postorderTraversal(NODE *pointer) {
+  if (pointer != NULL) {
+    postorderTraversal(pointer->left);
+    postorderTraversal(pointer->right);
+    printf("%d ", pointer->num);
   }
 }
 int main() {
-  insertNewNode(); // e.g., 50
-  insertNewNode(); // e.g., 30
-  insertNewNode(); // e.g., 70
-  printf("Inorder: ");
-  printInorder(ROOT);
+  NODE *ROOT = NULL;
+  insertNewNode(&ROOT); // e.g., 50
+  insertNewNode(&ROOT); // e.g., 30
+  insertNewNode(&ROOT); // e.g., 70
+  printf("\nInorder: ");
+  inorderTraversal(ROOT);
+  // printf("\nPreorder: ");
+  // preorderTraversal(ROOT);
+  // printf("\nPostorder: ");
+  // postorderTraversal(ROOT);
+  printf("\n");
+  deleteNodeCaller(&ROOT);
+  printf("\n");
+  printf("\nInorder: ");
+  inorderTraversal(ROOT);
   printf("\n");
   return 0;
 }
